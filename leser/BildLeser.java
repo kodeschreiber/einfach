0314891a0fc505ad671e6aed79fcbc01089dffa7
kodeschreiber <jbjansen97@gmail.com>
2016-11-23 19:07:31 -0500

    Update BildLeser.java

--- leser/BildLeser.java
+++ leser/BildLeser.java
@@ -1,6 +1,7 @@
 package net.einfach.leser;
 
-import java.awt.image.BufferedImage
+import java.awt.image.BufferedImage;
+import java.awt.Color;
 
 public class BildLeser
 {
@@ -11,6 +12,11 @@ public class BildLeser
             ExceptionLogger.add(new ArithmeticException("Letter is not a valid hex character"));
             return bi;
         }
+        
+        int grauInt = Integer.parseInt(grau + grau + "", "16");
+        for(int i = 0; i < bi.getWidth(); i++)
+            for(int j = 0; j < bi.getHeight(); j++)
+                if()
     }
     
     public static BufferedImage keineSchlange(BufferedImage bi)
@@ -26,5 +32,11 @@ public class BildLeser
     public static boolean isHex(char c)
     {
         return Character.isNumber(c) || Character.isLetter(c);
+    }
+    
+    public static boolean nebenWeiss(int grau, int farben)
+    {
+        Color col = new Color(color);
+        //TODO Comparators
     }
 }
